39a9ef80a2f8a8382df880caaeba816ee6125613
Carmen chan <carmen.chan@gtomato.com>
2019-06-19 22:52:30 +0800

    INIT main.go

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/clsung/plurgo/plurkgo"
+	"io/ioutil"
+	"log"
+)
+
+var credPath = flag.String("config", "config.json", "Path to configuration file containing the application's credentials.")
+
+func main() {
+	flag.Parse()
+	plurkOAuth, err := plurgo.ReadCredentials(*credPath)
+	if err != nil {
+		log.Fatalf("Error reading credential, %v", err)
+	}
+	accessToken, authorized, err := plurgo.GetAccessToken(plurkOAuth)
+
+	if authorized {
+		bytes, err := json.MarshalIndent(plurkOAuth, "", "  ")
+		if err != nil {
+			log.Fatalf("failed to store credential: %v", err)
+		}
+		err = ioutil.WriteFile(*credPath, bytes, 0700)
+		if err != nil {
+			log.Fatalf("failed to write credential: %v", err)
+		}
+	}
+
+	result, err := plurgo.CallAPI(accessToken, "/APP/Profile/getOwnProfile", map[string]string{})
+	if err != nil {
+		log.Fatalf("failed: %v", err)
+	}
+	fmt.Println(string(result))
+
+	var data = map[string]string{}
+	data["content"] = "Hello world form bbg bot"
+	data["qualifier"] = "shares"
+	data["lang"] = "ja"
+	result, err = plurgo.CallAPI(accessToken, "/APP/Timeline/plurkAdd", data)
+	if err != nil {
+		log.Fatalf("failed: %v", err)
+	}
+	fmt.Println(string(result))
+}
