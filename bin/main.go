0604258090d0f2de716f572b4a3cb8c28d6f839b
Carmen chan <carmen.chan@gtomato.com>
2019-06-20 21:28:51 +0800

    ADD cronjob

--- bin/main.go
+++ bin/main.go
@@ -1,12 +1,26 @@
 package main
 
-import plurkAction "github.com/chlorella/plurk-bbg-bot/src/pkg"
+import (
+	plurkAction "github.com/chlorella/plurk-bbg-bot/src/pkg"
+	"github.com/robfig/cron"
+	"log"
+)
 
 func main() {
-	accessToken, _ := plurkAction.GetPlurkOauth()
+	c := cron.New()
+	accessToken, err := plurkAction.GetPlurkOauth()
 
-	plurkAction.GetProfile(accessToken)
+	profile := plurkAction.GetProfile(accessToken)
+	log.Print(profile["alerts_count"])
+
+	if err != nil{
+		c.AddFunc("@hourly", func() {plurkAction.AddAllAsFriend(accessToken)})
+	}
+	c.Start()
 	//plurkAction.AddAllAsFriend(accessToken)
-	plurkAction.SendPlurk(accessToken, "(dice10)(dice)", "plays")
+	//plurkAction.SendPlurk(accessToken, "(dice10)(dice)", "plays")
+	defer c.Stop()
+
+	select{}
 }
 
