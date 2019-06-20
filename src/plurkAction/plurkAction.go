900992c160ef5591d0d9a1251335129de01a06ee
Carmen chan <carmen.chan@gtomato.com>
2019-06-20 15:03:30 +0800

    Rename plurkAction package

--- src/plurkAction/plurkAction.go
+++ src/plurkAction/plurkAction.go
@@ -1,16 +1,41 @@
-package pkg
+package plurkAction
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/clsung/plurgo/plurkgo"
 	"github.com/garyburd/go-oauth/oauth"
+	"io/ioutil"
 	"log"
 )
 
 var credPath = flag.String("config", "config.json", "Path to configuration file containing the application's credentials.")
 var lang = "ja"
 
+func getPlurkOauth() (*oauth.Credentials, error) {
+	flag.Parse()
+	plurkOAuth, err := plurgo.ReadCredentials(*credPath)
+	if err != nil {
+		log.Fatalf("Error reading credential, %v", err)
+		return nil, err
+	}
+	accessToken, authorized, err := plurgo.GetAccessToken(plurkOAuth)
+
+	if authorized {
+		bytes, err := json.MarshalIndent(plurkOAuth, "", "  ")
+		if err != nil {
+			log.Fatalf("failed to store credential: %v", err)
+			return nil, err
+		}
+		err = ioutil.WriteFile(*credPath, bytes, 0700)
+		if err != nil {
+			log.Fatalf("failed to write credential: %v", err)
+			return nil, err
+		}
+	}
+	return accessToken, nil
+}
 
 func getProfile (accessToken *oauth.Credentials){
 	result, err := plurgo.CallAPI(accessToken, "/APP/Profile/getOwnProfile", map[string]string{})
