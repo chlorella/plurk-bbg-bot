5874285ef9f3941aa0e7493e0a9ced3bcb58fc31
Carmen chan <carmen.chan@gtomato.com>
2019-06-20 15:22:18 +0800

    UPDATE main

--- bin/main.go
+++ bin/main.go
@@ -5,6 +5,8 @@ import plurkAction "github.com/chlorella/plurk-bbg-bot/src/pkg"
 func main() {
 	accessToken, _ := plurkAction.GetPlurkOauth()
 
-	plurkAction.GetPro()
+	plurkAction.GetProfile(accessToken)
+	//plurkAction.AddAllAsFriend(accessToken)
+	plurkAction.SendPlurk(accessToken, "(dice10)(dice)", "plays")
 }
 
